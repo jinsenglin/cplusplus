438831635d2a3b925c6aa499957a726428fef744
Jim Lin <jimlintw922@gmail.com>
2017-10-08 12:53:04 +0800

    update example 05

--- 05.class/main.cpp
+++ 05.class/main.cpp
@@ -22,11 +22,26 @@ class Person {
         }
 };
 
+class SubPerson : public Person {
+    public:
+        string vendor;
+
+        // constructor
+        SubPerson (int a, string g, string v) : Person(a, g) {
+            vendor = v;
+        }
+};
+
 int main() {
 
     Person yuyu (25, "female");
     cout << "Gender: " << yuyu.gender << endl;
     cout << "Age: " << yuyu.getAge() << endl;
    
+    SubPerson david (35, "male", "Weyland Corporation");
+    cout << "Gender: " << david.gender << endl;
+    cout << "Age: " << david.getAge() << endl;
+    cout << "Vendor: " << david.vendor << endl;
+
     return 0; 
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cplusplus
+
+# Reference
+
+* http://disp.ee.ntu.edu.tw/class/C++%E7%89%A9%E4%BB%B6%E5%B0%8E%E5%90%91%E5%8F%8A%E5%A2%9E%E9%80%B2%E6%95%88%E7%8E%87%E7%A8%8B%E5%BC%8F%E6%8A%80%E5%B7%A7
