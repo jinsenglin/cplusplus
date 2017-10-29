95dfdf293212364c9f21a902a6c5373edab6d79b
Jim Lin <jimlintw922@gmail.com>
2017-10-29 19:20:17 +0800

    update 06.collinear

--- c/06.collinear/main.c
+++ c/06.collinear/main.c
@@ -15,47 +15,47 @@ int main(){
   int T;
   scanf("%d", &T);
   for(int t=0;t<T;t++) {
-
-  int i,j,k,n,count=0;
-  int x[101],y[101],z[101];
-  scanf("%d",&n);
-  for(i=0;i<n;i++){
-      scanf("%d",&x[i]);
-      scanf("%d",&y[i]);
-      z[i] = 0;
-  }
   
-  //3 finite loops to compare any 2 points with the rest points
-  for(i=0;i<n;i++)
-  {
-    if (z[i] == 1) continue;
-
-    for(j=i+1;j<n;j++)
+    int i,j,k,n,count=0;
+    int x[101],y[101],z[101];
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&x[i]);
+        scanf("%d",&y[i]);
+        z[i] = 0;
+    }
+    
+    //3 finite loops to compare any 2 points with the rest points
+    for(i=0;i<n;i++)
     {
-      if (z[j] == 1) continue;
-
-      //find dots on the same vector between dots[i] and dots[j]
-      for(k=0;k<n;k++)
-      {
-        //printf("DEBUG: i=%d, j=%d, k=%d\n", i, j, k);
-
-        if(k==i||k==j) continue;
-        // (a.x - b.x) * (a.y - b.y) == (c.x - b.x) * (c.y - b.y)
-        else if((x[i]-x[j])*(y[k]-y[j])==(x[k]-x[j])*(y[i]-y[j]))
-        {
-          if (z[i] == 0 || z[j] == 0) count++;
-
-          z[i] = 1;
-          z[j] = 1;
-          z[k] = 1;
-
-          //printf("DEBUG: count++ i=%d, j=%d, k=%d\n", i, j, k);
-        }       
-      }
-    } 
-  }
+      if (z[i] == 1) continue;
   
-  printf("%d\n",count);
+      for(j=i+1;j<n;j++)
+      {
+        if (z[j] == 1) continue;
+  
+        //find dots on the same vector between dots[i] and dots[j]
+        for(k=0;k<n;k++)
+        {
+          //printf("DEBUG: i=%d, j=%d, k=%d\n", i, j, k);
+  
+          if(k==i||k==j) continue;
+          // (a.x - b.x) * (a.y - b.y) == (c.x - b.x) * (c.y - b.y)
+          else if((x[i]-x[j])*(y[k]-y[j])==(x[k]-x[j])*(y[i]-y[j]))
+          {
+            if (z[i] == 0 || z[j] == 0) count++;
+  
+            z[i] = 1;
+            z[j] = 1;
+            z[k] = 1;
+  
+            //printf("DEBUG: count++ i=%d, j=%d, k=%d\n", i, j, k);
+          }       
+        }
+      } 
+    }
+    
+    printf("%d\n",count);
   }
 
   return 0;  
