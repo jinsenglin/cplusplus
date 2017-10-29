c3a4eb1a3388f7f749aaa9d55b34baa44d007169
Jim Lin <jimlintw922@gmail.com>
2017-10-29 21:27:58 +0800

    add 07.exquisite-substrings

--- c/07.exquisite-substrings/main.c
+++ c/07.exquisite-substrings/main.c
@@ -1,5 +1,51 @@
 #include<stdio.h>
+#include<string.h>
+
+/*
+Input
+There are multiple lines in each testcase. Each line contains a string si that HT wrote on the paper.
+
+The input file is ended by 'EOF'.
+
+It is guaranteed that :
+
+At most 10 lines in each testcase.
+testcase #1 ~ #4 : 1 ≤ | s | ≤ 200
+testcase #5 Bonus: 1 ≤ | s | ≤ 2000
+*/
+
+int isPalindrome(char *str, int l, int r) {
+    return str[l] == str[r];
+}
+
+void run_testcase(char *str) {
+    unsigned long len = strlen(str);
+    //printf("DEBUG: len = %lu\n", len);
+
+    int number_of_exquisite_substrings = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i+1; j < len; j++) {
+            if ( isPalindrome(str, i, j) ) {
+                number_of_exquisite_substrings++;
+                //printf("DEBUG: number of exquisite substrings ++\n");
+            }
+        }
+    }
+
+    printf("%d\n", number_of_exquisite_substrings);
+}
+
+void load_testdata(char *str) {
+    while (scanf("%s", str) != EOF) {
+        //printf("DEBUG: str = %s\n", str);
+        //printf("DEBUG: len(str) = %lu\n", strlen(str));
+        run_testcase(str);
+    }
+}
 
 int main(){
-  return 0;  
+    char str[2001];
+    load_testdata(str);
+
+    return 0;  
 }
