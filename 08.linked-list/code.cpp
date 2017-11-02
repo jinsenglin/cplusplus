9f61f4a1cb5290baed33c06a91ace07b8a52c1ac
Jim Lin <jimlintw922@gmail.com>
2017-11-02 20:57:02 +0800

    add 08.linked-list

---
+++ 08.linked-list/code.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include "function.h"
+
+
+void Implement::InsertBack(int data)
+{
+    
+}
+
+void Implement::InsertFront(int data)
+{
+    
+}
+
+void Implement::InsertAfter(int data1, int data2)
+{
+    
+}
+
+void Implement::Delete(int data)
+{
+    
+}
+
+void Implement::DeleteFront()
+{
+    
+}
+
+void Implement::DeleteBack()
+{
+    
+}
+
+void Implement::Reverse()
+{
+    
+}
