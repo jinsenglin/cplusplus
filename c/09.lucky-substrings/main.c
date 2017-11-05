92c973d67a99609aad77a6150f37de29d808f29e
Jim Lin <jimlintw922@gmail.com>
2017-11-05 12:52:13 +0800

    udpate 09.lucky-substrings

--- c/09.lucky-substrings/main.c
+++ c/09.lucky-substrings/main.c
@@ -22,22 +22,22 @@ For each string si, please output a line contains one integer representing the n
 */
 
 void run_testcase(char *str) {
-    printf("DEBUG: str = %s\n", str);
+    //printf("DEBUG: str = %s\n", str);
 
     unsigned long len = strlen(str);
-    printf("DEBUG: len(str) = %lu\n", len);
+    //printf("DEBUG: len(str) = %lu\n", len);
 
     // TODO prepare an array to store all indeices of '9' character
     for (int i=0; i<len; i++) {
         char c = str[i];
-        printf("DEBUG: c = %c\n", c);
+        //printf("DEBUG: c = %c\n", c);
 
         if (c == '9') {
             // TODO
         }
     }
 
-    // TODO list all substrings
+    // list all substrings
     //
     // for a string of length N
     // it has 1 substring of length N-0
@@ -46,33 +46,48 @@ void run_testcase(char *str) {
     // ...
     // it has N substring of length 1
     //
+    //
+    // the first and last characters of the 1st substring of length N are
+    // 0, (N-1)
+    //
     // the first and last characters of the 1st substring of length N-1 are
-    // 0, n-1-1
+    // 0, (N-1)-1
     // the first and last characters of the 2nd substring of length N-1 are
-    // 1, n-1-0
+    // 1, (N-1)-0
     //
     // the first and last characters of the 1st substring of length N-2 are
-    // 0, n-1-2
+    // 0, (N-1)-2
     // the first and last characters of the 2nd substring of length N-2 are
-    // 1, n-1-1
+    // 1, (N-1)-1
     // the first and last characters of the 3rd substring of length N-2 are
-    // 2, n-1-0
-    for (int i=0; i<len; i++) {
-        int first = 0;
-        int last = len-1-i;
+    // 2, (N-1)-0
+    int N = len;
+    for (int i=0; i<N; i++) {
+        int first;
+        int eol;
 
-        for (int j=0; j<len-i; j++) {
-            // TODO shift first, last
-            // TODO print after shift
+        // shift first and last
+        printf("DEBUG: substring of length %D\n", i+1);
+        for (int j=0; j<N-i; j++) {
+            first = j;
+            eol = j+(i+1);
+
+            /*
+            printf("DEBUG: substring = ");
+            for (int k=first; k<eol; k++) {
+                printf("%c", str[k]);
+            }
+            printf("\n");*/
+
+            // TODO given a substring and a sorted array of [0, 5, 9] which means s[0] == s[5] == s[9] == '9'
+            // determine if the given substring has '9' character
+            // check first <= 0 <= end, if true return true else check next
+            // check first <= 5 <= end, if true return true else check next
+            // check first <= 9 <= end, if true return true else check next
+            // return false
         }
     }
  
-    // TODO given a substring and a sorted array of [0, 5, 9] which means s[0] == s[5] == s[9] == '9'
-    // determine if the given substring has '9' character
-    // check first <= 0 <= end, if true return true else check next
-    // check first <= 5 <= end, if true return true else check next
-    // check first <= 9 <= end, if true return true else check next
-    // return false
 
 }
 
