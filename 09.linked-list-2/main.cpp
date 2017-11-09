c2c81877bf50f08ababf1b4f74877c34a5967bb0
Jim Lin <jimlintw922@gmail.com>
2017-11-09 19:20:15 +0800

    reset 09.linked-list-2

--- 09.linked-list-2/main.cpp
+++ 09.linked-list-2/main.cpp
@@ -1,75 +1,14 @@
- //========================DO-NOT-MODIFY-THE-FILE========================
 #include <iostream>
-#include <cstring>
-#include "function.h"
 
+using namespace std; 
 
-void tryTestCase(Chain &inst)
-{
-    /*FILE * pFile;
-    pFile = fopen(testfile.c_str(), "r");
-    //std::ifstream ifs(testfile);
+int main() {
 
-    if( !pFile )
-        throw "[Missing Test File]";*/
+    // print the character "end of line"
+    cout << endl;
 
-    // parse each case
-    char op[15];
-    char result[2000];
-    int value1, value2;
-    bool isPass = false;
-    while(std::cin >> op)
-    {
-        if(strcmp("InsertBack", op) == 0){
-            std::cin >> value1;
-            //std::cout << "InsertBack " << value1 << endl;
-            inst.InsertBack(value1);
-        }
-		if(strcmp("InsertFront", op) == 0){
-            std::cin >> value1;
-            inst.InsertFront(value1);
-        }
-        else if(strcmp("InsertAfter", op) == 0){
-            /*fscanf(pFile, "%d", &value1);
-            fscanf(pFile, "%d", &value2);*/
-            std::cin >> value1 >> value2;
-            //std::cout << "InsertAfter " << value1 << " " << value2 <<endl;
-            inst.InsertAfter(value1, value2);
-        }
-        else if(strcmp("Delete", op) == 0){ 
-            //scanf(pFile, "%d", &value1);
-            std::cin >> value1;
-            //std::cout << "Delete " << value1 << endl;
-            inst.Delete(value1);
-        }
-		else if(strcmp("DeleteFront", op) == 0){ 
-            inst.DeleteFront();
-        }
-		else if(strcmp("DeleteBack", op) == 0){ 
-            inst.DeleteBack();
-        }
-        else if(strcmp("Reverse", op) == 0) {
-            //std::cout << "Reverse " << endl;
-            inst.Reverse();
-        }
-        else if(strcmp("End", op) == 0) {
-            //fscanf(pFile, "%s", result);
-            //cin >> result;
-            string urResultStr = inst.toString();
-            std::cout << urResultStr << endl;
-            //std:: cout << result << endl;
-            // compare results
-            //isPass = strcmp(urResultStr.c_str(), result);
-            break;
-        }
-    }
-
+    // print the string "Hello World" and the character "end of line"
+    cout << "Hello World" << endl;
+   
+    return 0; 
 }
-int main(int argc, char *argv[])
-{
-    Implement inst = *(new Implement());
-    tryTestCase(inst);
-	return 0;
-}
-
-//========================DO-NOT-MODIFY-THE-FILE======================== 
