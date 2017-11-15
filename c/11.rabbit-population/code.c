d4a02cc11320f31ba61d6f835b0990050d6e3f75
Jim Lin <jimlintw922@gmail.com>
2017-11-15 16:54:26 +0800

    update code.c

--- c/11.rabbit-population/code.c
+++ c/11.rabbit-population/code.c
@@ -20,7 +20,7 @@ int Rabbit(int month)
 {
 /*
                     *
-0   1   2   3   4   5   6
+1   2   3   4   5   6   7
 2   2
                         2
                     2   2
@@ -35,10 +35,13 @@ int Rabbit(int month)
                 2   2
                         2
 -------------------------
-    1   2   3   5   8   13
-       +1  +1  +2  +3  +5
+1   1   2   3   5   8   13
+      1+1 1+2 2+3 3+5 5+8
 */
 
     // Write your code here
+    if ( month == 1 ) return 1;
+    else if (month == 2) return 1;
+    else return Rabbit(month-1) + Rabbit(month-2);
 
 }
