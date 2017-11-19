4c60c65782b44f93336a6439fa8ebab3ab721d93
Jim Lin <jimlintw922@gmail.com>
2017-11-19 23:02:03 +0800

    udpate 11.max-heap

--- 11.max-heap/code.cpp
+++ 11.max-heap/code.cpp
@@ -5,24 +5,30 @@
 using namespace std;
 
 void Implement::Insert(int value) {
+    // TODO
 }
 
 void Implement::DeleteMax() {
+    // TODO
 }
 
 int Implement::MaxPathWeight(int index) {
+    // TODO
     return 0;
 }
 
 string Implement::InorderTraversal(int index) {
+    // TODO
     return "";
 }
 
 string Implement::PreorderTraversal(int index) {
+    // TODO
     return "";
 }
 
 string Implement::PostorderTraversal(int index) {
+    // TODO
     return "";
 }
 
