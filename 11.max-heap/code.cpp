f104c22a5cf30a3299dc0fa027f40dffeb11ab9c
Jim Lin <jimlintw922@gmail.com>
2017-11-19 23:01:12 +0800

    update 11.max-heap

---
+++ 11.max-heap/code.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "function.h"
+#include <cstring>
+
+using namespace std;
+
+void Implement::Insert(int value) {
+}
+
+void Implement::DeleteMax() {
+}
+
+int Implement::MaxPathWeight(int index) {
+    return 0;
+}
+
+string Implement::InorderTraversal(int index) {
+    return "";
+}
+
+string Implement::PreorderTraversal(int index) {
+    return "";
+}
+
+string Implement::PostorderTraversal(int index) {
+    return "";
+}
+
