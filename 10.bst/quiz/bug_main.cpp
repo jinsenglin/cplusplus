a5afbfbdc638310639513e5dbae7db0ae0dc7501
Jim Lin <jimlintw922@gmail.com>
2017-11-22 20:48:31 +0800

    update bug_main.cpp

--- 10.bst/quiz/bug_main.cpp
+++ 10.bst/quiz/bug_main.cpp
@@ -10,14 +10,16 @@ friend class BST;
 
 public:
     TreeNode(int value) {
-        //int data;
+        int data = value; // BUG !!!!
+        // 1. this->data = value;
+        // 2. data = value;
         this->leftChild = NULL;
         this->rightChild = NULL;
         //this->leftSize = 0;
     }
 
     TreeNode(int value, const TreeNode & left, const TreeNode & right) {
-      //int data;
+        int data = value;
         this->leftChild = new TreeNode(left);
         this->rightChild = new TreeNode(right);
     }
@@ -58,33 +60,29 @@ public:
 
 
     void Insert(int value) {
-        // Search for key “thePair.first”, pp is the parent of p
-        //If the search is unsuccessful, then the element is inserted at the point the search terminates
-        TreeNode* p = root, *pp = 0;
-        TreeNode *insert_node = new TreeNode(value);
-        while (p) {
-            pp = p;
-            if (value < p->data) p = p->leftChild;
-            else if (value > p->data) p = p->rightChild;
-            else {
-                // Duplicate, update the value of element
-                p->data = value;
+            // Search for key “thePair.first”, pp is the parent of p
+            //If the search is unsuccessful, then the element is inserted at the point the search terminates
+            TreeNode* p = root, *pp = 0;
+            while (p) {
+                pp = p;
+                if (value < p->data) p = p->leftChild;
+                else if (value > p->data) p = p->rightChild;
+                else {
+                    // Duplicate, update the value of element
+                    p->data = value;
                 return;
+                }
             }
-        }
 
-        // Perform the insertion
-        insert_node->parent = pp;
-        if (pp == NULL){                     // 下面一組if-else, 把insert_node接上BST
-          this->root = insert_node;
+            // Perform the insertion
+            p = new TreeNode(value);
+            if (root) {
+                // tree is not empty
+                if (value < pp->data) pp->leftChild = p;
+                else pp->rightChild = p;
+            }
+            else root = p;
         }
-        else if (insert_node->data < pp->data){
-            pp->leftChild = insert_node;
-        }
-        else{
-            pp->rightChild = insert_node;
-        }
-    }
 
     void Delete(int k) {
         // TODO
