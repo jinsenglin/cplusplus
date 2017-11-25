6eb10d141f7ccf44fecd6cc224ffa44fb22b4780
Jim Lin <jimlintw922@gmail.com>
2017-11-25 15:19:13 +0800

    update 15.firework-show

--- c/15.firework-show/main.c
+++ c/15.firework-show/main.c
@@ -41,10 +41,57 @@ Sample Output
 3
 */
 
+// ===============================================
+// Source: https://ide.geeksforgeeks.org/index.php
+
+void combinationUtil(int arr[], int data[], int start, int end, int index, int r);
+
+// The main function that prints all combinations of size r
+// in arr[] of size n. This function mainly uses combinationUtil()
+void printCombination(int arr[], int n, int r)
+{
+    // A temporary array to store all combination one by one
+    int data[r];
+
+    // Print all combination using temprary array 'data[]'
+    combinationUtil(arr, data, 0, n-1, 0, r);
+}
+
+/* arr[] ---> Input Array
+data[] ---> Temporary array to store current combination
+start & end ---> Staring and Ending indexes in arr[]
+index ---> Current index in data[]
+r ---> Size of a combination to be printed */
+void combinationUtil(int arr[], int data[], int start, int end, int index, int r)
+{
+    // Current combination is ready to be printed, print it
+    if (index == r)
+    {
+        for (int j=0; j<r; j++)
+            printf("%d ", data[j]);
+        printf("\n");
+        return;
+    }
+
+    // replace index with all possible elements. The condition
+    // "end-i+1 >= r-index" makes sure that including one element
+    // at index will make a combination with remaining elements
+    // at remaining positions
+    for (int i=start; i<=end && end-i+1 >= r-index; i++)
+    {
+        data[index] = arr[i];
+        combinationUtil(arr, data, i+1, end, index+1, r);
+    }
+}
+
+// ===============================================
+
 void run_testcase(int *m, int *data) {
     //printf("DEBUG: m = %d\n", *m);
 
     int result = -1;
+
+    
 
     printf("%d\n", result);
 }
@@ -64,7 +111,7 @@ void load_testdata(int *T, int *n, int *m, int data[]) {
 
         for (int i=0; i<*n; i++) {
             scanf("%d", &data[i]);
-            printf("DEBUG: i = %d, a[i] = %d\n", i, data[i]);
+            //printf("DEBUG: i = %d, a[i] = %d\n", i, data[i]);
         }
 
         run_testcase(m, data);
