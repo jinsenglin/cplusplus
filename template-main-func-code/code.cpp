794613a4156523b3c0ecc5d05a06110010013c34
Jim Lin <jimlintw922@gmail.com>
2017-11-26 21:56:30 +0800

    add 12.graph template-main-func-code

---
+++ template-main-func-code/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "function.h"
+#include <cstring>
+
+using namespace std;
+
+void Implement::addEdge(const int label_1, const int label_2 , const int weight) {}
+void Implement::deleteEdge(const int label_1, const int label_2) {}
+void Implement::deleteVertex(const int label) {}
+int Implement::degree(const int label) {return 0;}
+bool Implement::isExistPath(const int label_1, const int label_2) {return false;}
+void Implement::deleteGraph() {}
+int Implement::number_of_component() {return 0;}
+bool Implement::isExistCycle() {return false;}
