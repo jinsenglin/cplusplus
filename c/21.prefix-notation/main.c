995e405abb1463d78be0431abccda92b00b6790c
Jim Lin <jimlintw922@gmail.com>
2017-12-02 23:08:32 +0800

    add 21.prefix-notation

---
+++ c/21.prefix-notation/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+Input
+An equation in prefix notation.
+
+Operands are integers in the range between 0 and 9.
+
+There are four operators in this problem: addition('+'), subtraction('-'), multiplication('*'), and division('/').
+
+At the end of an equation there is a '=' symbol. 
+
+No blank space between elements.
+
+No calculate errors(for example, 5/0) will happen.
+
+The length of equations (includes '=') is less than 40.
+
+Output
+Show the answer of equation
+
+Note that the answer may be a decimal number, so you are asked to use '%.3f' to show the answer, or you'll get WA even if your answer is correct.
+
+No new line character at the end. 
+
+Sample Input
+/4*2+13=
+
+Sample Output
+0.500
+*/
+int main() {
+}
