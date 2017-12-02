0b24d2e3de471475b567e1e59a7fd685da6b6e20
Jim Lin <jimlintw922@gmail.com>
2017-12-02 15:15:27 +0800

    update 19.pc-ponds

--- c/19.pc-ponds/main.c
+++ c/19.pc-ponds/main.c
@@ -33,11 +33,69 @@ int main() {
         }
     }
 
+    int count = 0;
+    for (int i=0; i<m; i++) {
+        for( int j=0; j<n; j++) {
+            if (map[i][j] == '~') {
+                if (i == 0) {
+                    if (j == 0) {
+                        if (map[i][j+1] == '~') {count++; continue;}
+                        if (map[i+1][j] == '~') {count++; continue;}
+                    }
+                    else if (j == n-1) {
+                        if (map[i][j-1] == '~') {count++; continue;}
+                        if (map[i+1][j] == '~') {count++; continue;}
+                    }
+                    else {
+                        if (map[i][j-1] == '~') {count++; continue;}
+                        if (map[i][j+1] == '~') {count++; continue;}
+                        if (map[i+1][j] == '~') {count++; continue;}
+                    }
+                }
+                else if (i == m-1) {
+                    if (j == 0) {
+                        if (map[i][j+1] == '~') {count++; continue;}
+                        if (map[i-1][j] == '~') {count++; continue;}
+                    }
+                    else if (j == n-1) {
+                        if (map[i][j-1] == '~') {count++; continue;}
+                        if (map[i-1][j] == '~') {count++; continue;}
+                    }
+                    else {
+                        if (map[i][j-1] == '~') {count++; continue;}
+                        if (map[i][j+1] == '~') {count++; continue;}
+                        if (map[i-1][j] == '~') {count++; continue;}
+                    }
+                }
+                else {
+                    if (j == 0) {
+                        if (map[i][j+1] == '~') {count++; continue;}
+                        if (map[i+1][j] == '~') {count++; continue;}
+                        if (map[i-1][j] == '~') {count++; continue;}
+                    }
+                    else if (j == n-1) {
+                        if (map[i][j-1] == '~') {count++; continue;}
+                        if (map[i+1][j] == '~') {count++; continue;}
+                        if (map[i-1][j] == '~') {count++; continue;}
+                    }
+                    else {
+                        if (map[i][j-1] == '~') {count++; continue;}
+                        if (map[i][j+1] == '~') {count++; continue;}
+                        if (map[i+1][j] == '~') {count++; continue;}
+                        if (map[i-1][j] == '~') {count++; continue;}
+                    }
+                }
+            }
+        }
+    }
+    printf("%d\n", count);
+       
     // dump map
+    /*
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
             printf("%c", map[i][j]);
         }
         printf("\n");
-    }
+    }*/
 }
