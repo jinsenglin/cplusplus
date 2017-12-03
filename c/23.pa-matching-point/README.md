fde26c73f15561ba704ca7d10508b0f7b4508ff5
Jim Lin <jimlintw922@gmail.com>
2017-12-03 20:02:19 +0800

    update 23.pa-matching-point

--- c/23.pa-matching-point/README.md
+++ c/23.pa-matching-point/README.md
@@ -1,1 +1,4 @@
 https://acm.cs.nthu.edu.tw/contest/1342/#problem11692
+
+REF
+- https://stackoverflow.com/questions/20347170/char-array-and-char-array
--- c/23.pa-matching-point/main.c
+++ c/23.pa-matching-point/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 
+char s[] = "htisanaughtyboy";
+char t[] = "ht";
+
+int indexOf(int s_start, int t_size) {
+    for (int i=s_start; i<strlen(s)-t_size+1; i++) {
+        bool found = true;
+        for (int j=0; j<t_size; j++) {
+            if (t[j] != s[i+j]) {found = false; break;}
+        }
+        if (found) return i;
+    }
+
+    // return -1 if not found  
+    return -1;
+}
+
 int main() {
+    printf("DEBUG: %d\n", indexOf(0, 2));
 }
