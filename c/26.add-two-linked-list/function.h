cd339943d967eb6511ae425599aa32eba846df34
Jim Lin <jimlintw922@gmail.com>
2017-12-20 20:46:52 +0800

    update 26

--- c/26.add-two-linked-list/function.h
+++ c/26.add-two-linked-list/function.h
@@ -10,3 +10,13 @@ void Create_List(Node**, int);
 Node* Add_List(Node*, Node*);
 void Print_List(Node*);
 void Free_List(Node*);
+
+void Create_List(Node** head, int data) {}
+
+Node* Add_List(Node* head1, Node* head2) {
+    return NULL;
+}
+
+void Print_List(Node* head) {}
+
+void Free_List(Node* head){}
