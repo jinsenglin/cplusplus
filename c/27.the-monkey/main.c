a160a431c9580f364aaf870b19a6e7ae990416fd
Jim Lin <jimlintw922@gmail.com>
2017-12-24 21:51:12 +0800

    update 27

--- c/27.the-monkey/main.c
+++ c/27.the-monkey/main.c
@@ -27,17 +27,41 @@ void Create_List(Node** head, char data) {
     }
 }
 
-void delete_node() {
+void delete_node(Node** cursor) {
 }
 
-void insert_node() {
+void insert_node(Node** cursor, char data) {
 }
 
-void print_list() {
+void print_list(Node** head) {
 }
 
 int main() {
     Node *head = NULL;
+    Node *cursor = NULL;
     char data = 'c';
     Create_List(&head, data);
+
+
+    // read one character at one time
+    while ((data = getchar()) != '\n') {
+        if (data == 'D') {
+            // The capital letter ‘D’ represents that Evan deletes a character. If there is no character after the cursor, it does nothing.
+            delete_node(&cursor);
+        }
+        else if (data == '<') {
+            // Character ‘<’ represents that Evan left shifts the cursor. If the cursor is located at the front of the sentence, it does nothing.
+        }
+        else if (data == '>') {
+            // Character ‘>’ represents that Evan right shifts the cursor. If the cursor is located at the back of the sentence, it does nothing.
+        }
+        else {
+            // The lowercase letter from ‘a’ to ‘z’ represents that Evan types a lowercase character (same as your record).
+            // The underline symbol ‘_' represents that Evan types ‘_’.
+            insert_node(&cursor, data);
+        }
+    }
+
+    // print the result
+    print_list(&head);
 }
