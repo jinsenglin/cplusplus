07b5b5946cc2bded55f740a7420159efa1caa2d1
Jim Lin <jimlintw922@gmail.com>
2017-12-24 20:23:01 +0800

    update 28

--- c/28.the-great-depression/main.c
+++ c/28.the-great-depression/main.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
 
+typedef struct _Factory {
+    char name[21];  // factory name
+//    int a;          // profit of producing car A
+//    int b;          // profit of producing car B
+    int x;          // x = a-b
+} Factory;
+
 int main() {
+    int n;
+    int x;
+    int y;
+    scanf("%d %d %d", &n, &x, &y);
+
+    Factory factories[n];
+    for (int i=0; i<n; i++) {
+        int a, b;
+        scanf("%s %d %d", factories[i].name, &a, &b);
+        factories[i].x = a-b;
+    }
 }
