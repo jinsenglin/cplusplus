20ecfdbf58abb9876024a87950e3a173b328b0a3
Jim Lin <jimlintw922@gmail.com>
2017-12-24 21:19:39 +0800

    update 27

--- c/27.the-monkey/main.c
+++ c/27.the-monkey/main.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Node {
+    char data;
+    struct _Node *next;
+} Node;
+
+void Create_List(Node** head, char data) {
+    if (*head == NULL) {
+        *head = malloc(sizeof(Node));
+        (*(*head)).data = data;
+    }
+    else {
+        //printf("DEBUG: function.h :: %d\n", (*(*head)).data);
+        Node *prev;
+        Node *current = *head;
+
+        while (current != NULL) {
+            prev = current;
+            //printf("DEBUG: function.h :: %d\n", (*current).data);
+            current = (*current).next;
+        }
+       
+        (*prev).next = malloc(sizeof(Node));
+        (*((*prev).next)).data = data; 
+    }
+}
 
 int main() {
+    Node *head = NULL;
+    char data = 'c';
+    Create_List(&head, data);
 }
