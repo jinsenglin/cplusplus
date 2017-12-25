87ebd3bfa46c911994560591532394c349bb5b8d
Jim Lin <jimlintw922@gmail.com>
2017-12-25 14:02:50 +0800

    update malloc-free

--- c/malloc-free/main.c
+++ c/malloc-free/main.c
@@ -6,10 +6,40 @@ typedef struct _Node {
     struct _Node* next;
 } Node;
 
-void fn() {
+void fn(Node** list) {
+    Node* head = malloc(sizeof(Node));
+    head->c = 'c';
+    head->next = head;
+    printf("&head = %p\n", &head);
+    printf("head = %p\n", head);
+    printf("(*head).c = %c\n", (*head).c);
+    printf("head->c = %c\n", head->c);
+    printf("head->next = %p\n", head->next);
+    printf("head->next->c = %c\n", head->next->c);
+
+    Node* cursor = head;
+    printf("&cursor = %p\n", &cursor);
+    printf("cursor = %p\n", cursor);
+    printf("(*cursor).c = %c\n", (*cursor).c);
+    printf("cursor->c = %c\n", cursor->c);
+    printf("cursor->next = %p\n", cursor->next);
+    printf("cursor->next->c = %c\n", cursor->next->c);
+
+    *list = head;
+/*
+    printf("going to free(head)\n");
+    free(head);
+    head = NULL;
+
+    printf("note: can not double-free, which means free(cursor) is not allowed after free(head). so just make it NULL\n");
+    cursor = NULL;
+*/
 }
 
 int main() {
+    Node* list = NULL;
+    fn(&list);
+/*
     Node* head = malloc(sizeof(Node));
     head->c = 'c';
     head->next = head;
@@ -34,4 +64,5 @@ int main() {
 
     printf("note: can not double-free, which means free(cursor) is not allowed after free(head). so just make it NULL\n");
     cursor = NULL;
+*/
 }
