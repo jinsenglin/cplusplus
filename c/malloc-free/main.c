58effdd561a717cd54c5c41d1abcf9bef65717ab
Jim Lin <jimlintw922@gmail.com>
2017-12-25 13:54:53 +0800

    update malloc-free

--- c/malloc-free/main.c
+++ c/malloc-free/main.c
@@ -6,20 +6,27 @@ typedef struct _Node {
     struct _Node* next;
 } Node;
 
+void fn() {
+}
+
 int main() {
     Node* head = malloc(sizeof(Node));
     head->c = 'c';
     head->next = head;
     printf("&head = %p\n", &head);
     printf("head = %p\n", head);
+    printf("(*head).c = %c\n", (*head).c);
     printf("head->c = %c\n", head->c);
     printf("head->next = %p\n", head->next);
+    printf("head->next->c = %c\n", head->next->c);
 
     Node* cursor = head;
     printf("&cursor = %p\n", &cursor);
     printf("cursor = %p\n", cursor);
+    printf("(*cursor).c = %c\n", (*cursor).c);
     printf("cursor->c = %c\n", cursor->c);
     printf("cursor->next = %p\n", cursor->next);
+    printf("cursor->next->c = %c\n", cursor->next->c);
 
     printf("going to free(head)\n");
     free(head);
