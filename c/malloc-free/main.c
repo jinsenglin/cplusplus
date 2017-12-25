d157416129379119e90120e96955317cd7887b07
Jim Lin <jimlintw922@gmail.com>
2017-12-25 13:42:36 +0800

    add malloc-free

---
+++ c/malloc-free/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Node {
+    char c;
+    struct _Node* next;
+} Node;
+
+int main() {
+    Node* head = malloc(sizeof(Node));
+    head->c = 'c';
+    printf("&head = %p\n", &head);
+    printf("head = %p\n", head);
+    printf("head->c = %c\n", head->c);
+
+    Node* cursor = head;
+    printf("&cursor = %p\n", &cursor);
+    printf("cursor = %p\n", cursor);
+    printf("cursor->c = %c\n", cursor->c);
+
+    printf("going to free(head)\n");
+    free(head);
+    printf("head->c = %c\n", head->c);
+    printf("cursor->c = %c\n", cursor->c);
+
+    printf("done");
+}
