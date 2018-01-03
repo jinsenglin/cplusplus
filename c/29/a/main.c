a4043b44d3c04ae1332fc56d5d3d6b72d3dfdfda
Jim Lin <jimlintw922@gmail.com>
2018-01-03 21:41:40 +0800

    update 29.a

--- c/29/a/main.c
+++ c/29/a/main.c
@@ -13,8 +13,32 @@ int main() {
         int X[n];
         int p;
         for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                scanf("%f", &A[i][j]);
+            }
+        }
+        for (int i=0; i<n; i++) {
             scanf("%d", &X[i]);
         }
         scanf("%d", &p);
+
+        // next X
+        float MOVE[n][n];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                MOVE[i][j] = A[i][j] * X[j];
+            }
+        }
+        for (int i=0; i<n; i++) {
+            X[i] = 0;
+            for (int j=0; j<n; j++) {
+                X[i] += MOVE[i][j];
+            }
+        }
+
+        // [ DEBUG ] print X
+        for (int i=0; i<n; i++) {
+            printf("%d ", X[i]);
+        }
     }
 }
