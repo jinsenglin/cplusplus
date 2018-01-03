516a5c21090af60bd6198ea6bc21bf7cb0660814
Jim Lin <jimlintw922@gmail.com>
2018-01-03 21:27:19 +0800

    update 29.a

--- c/29/a/main.c
+++ c/29/a/main.c
@@ -2,4 +2,19 @@
 #include <stdbool.h>
 
 int main() {
+    int T = 0;
+    scanf("%d", &T);
+
+    for (int t=0; t<T; t++) {
+        int n = 2;
+        scanf("%d", &n);
+
+        float A[n][n];
+        int X[n];
+        int p;
+        for (int i=0; i<n; i++) {
+            scanf("%d", &X[i]);
+        }
+        scanf("%d", &p);
+    }
 }
