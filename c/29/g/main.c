6b3b598a86a9049b4aa191923acb53e56eab5910
Jim Lin <jimlintw922@gmail.com>
2018-01-05 23:47:13 +0800

    update g

--- c/29/g/main.c
+++ c/29/g/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int last_bit(int *num) {
+    int one_or_zero = *num - (*num >> 1)*2;
+    *num = *num >> 1;
+
+    return one_or_zero;
+}
+
 int main() {
     int n;
     scanf("%d", &n);
@@ -8,6 +15,9 @@ int main() {
     int a[100000] = { 0 };
     for (int i=0; i<n; i++) {
         scanf("%d", &a[i]);
-        printf("DEBUG: %d\n", a[i]);
+
+        while (a[i] > 0) {
+            printf("DEBUG: %d\n", last_bit(&a[i]));
+        }
     }
 }
