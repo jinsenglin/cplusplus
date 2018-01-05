f469147208954221679e81d2f1494e1d0a4077ee
Jim Lin <jimlintw922@gmail.com>
2018-01-05 23:19:34 +0800

    update g

--- c/29/g/main.c
+++ c/29/g/main.c
@@ -2,4 +2,12 @@
 #include <stdbool.h>
 
 int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[100000] = { 0 };
+    for (int i=0; i<n; i++) {
+        scanf("%d", &a[i]);
+        printf("DEBUG: %d\n", a[i]);
+    }
 }
