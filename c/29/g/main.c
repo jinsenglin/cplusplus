d4807e744d1b14e67871da497cbdd375b9f60fcc
Jim Lin <jimlintw922@gmail.com>
2018-01-06 00:04:16 +0800

    update g

--- c/29/g/main.c
+++ c/29/g/main.c
@@ -5,6 +5,7 @@ int last_bit(int *num) {
     int one_or_zero = *num - (*num >> 1)*2;
     *num = *num >> 1;
 
+    //printf("DEBUG: %d\n", one_or_zero);
     return one_or_zero;
 }
 
@@ -12,13 +13,22 @@ int main() {
     int n;
     scanf("%d", &n);
 
+    int bits[20] = { 0 };
     int a[100000] = { 0 };
     for (int i=0; i<n; i++) {
         scanf("%d", &a[i]);
 
         int ai = a[i];
+        int pos = 0;
         while (ai > 0) {
-            printf("DEBUG: %d\n", last_bit(&ai));
+            if (last_bit(&ai) == 1) bits[pos]++;
+            else bits[pos]--;
+
+            pos++;
         }
+
     }
+
+    // DEBUG: dump bits
+    for (int i=19; i>-1; i--) printf("%d ", bits[i]);
 }
