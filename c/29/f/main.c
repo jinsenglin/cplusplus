c50c71c60dfba9fc9509a43ec19529eac9678c11
Jim Lin <jimlintw922@gmail.com>
2018-01-06 22:29:25 +0800

    update f

--- c/29/f/main.c
+++ c/29/f/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+/*
 struct Edge {
     int from;
     int to;
-};
+};*/
 
 int main() {
     int T;
@@ -14,9 +14,16 @@ int main() {
         int n, m;
         scanf("%d %d", &n, &m);
 
-        struct Edge edges[1000000];
+        int pool1[1000] = { 0 };
+        int pool2[1000] = { 0 };
+//        struct Edge edges[1000000];
         for (int j=0; j<m; j++) {
-            scanf("%d %d", &edges[j].from, &edges[j].to);
+            int from, to;
+            scanf("%d %d", &from, &to);
+//            scanf("%d %d", &edges[j].from, &edges[j].to);
+
+            // analyze TODO
         }
+
     }
 }
