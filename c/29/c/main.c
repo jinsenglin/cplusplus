166a26e90673fbb7ab6d47e356e33c9df38e1d64
Jim Lin <jimlintw922@gmail.com>
2018-01-06 20:32:37 +0800

    update c

--- c/29/c/main.c
+++ c/29/c/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
+struct Record {
+    char name[21];
+    int score;
+};
 
 int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Record records[100000];
+    for (int i=0; i<n; i++) {
+        scanf("%s %d", records[i].name, &records[i].score);
+    }
+
 }
