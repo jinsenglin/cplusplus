b9b37edaf9eaa07e5b7af3bffab1a1a74611d8ae
Jim Lin <jimlintw922@gmail.com>
2018-01-07 15:22:28 +0800

    update f

--- c/29/f/main.c
+++ c/29/f/main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void update_matrix(int matrix[1000][1000], int u, int v) {
+    bool u_flag = false;
+    bool v_flag = false;
+
+    int i = 0;
+    for (; i<1000; i++) {
+        if (matrix[i][0] == 0) break;
+        else if (!u_flag && matrix[i][0] == u) {
+            u_flag = true;
+            for (int j=1; j<1000; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][j] = v;
+                    break;
+                }
+            }
+        }
+        else if (!u_flag && matrix[i][0] == v) {
+            v_flag = true;
+            for (int j=1; j<1000; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][j] = u;
+                    break;
+                }
+            }
+        }
+    }
+    if (!u_flag) {
+        matrix[i][0] = u;
+        matrix[i][1] = v;
+        i++;
+    }
+    if (!v_flag) {
+        matrix[i][0] = v;
+        matrix[i][1] = u;
+    }
+}
+
 int main() {
     int T;
     scanf("%d", &T);
@@ -14,8 +51,22 @@ int main() {
             int u, v;
             scanf("%d %d", &u, &v);
 
-            // update matrix TODO
+            // update matrix
+            update_matrix(matrix, u, v);
         }
+
+        // DEBUG: dump matrix
+        /*
+        for (int j=0; j<1000; j++) {
+            if (matrix[j][0] == 0) break;
+            else {
+                for (int k=0; k<1000; k++) {
+                    if (matrix[j][k] == 0) break;
+                    else printf("%d ", matrix[j][k]);
+                }
+                printf("\n");
+            }
+        }*/
 
         // analyze matrix TODO
     }
