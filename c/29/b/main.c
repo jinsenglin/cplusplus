ab9300f1d1bd9a9c6a36fb7d9910e24d85e98b96
Jim Lin <jimlintw922@gmail.com>
2018-01-07 17:14:43 +0800

    update b

--- c/29/b/main.c
+++ c/29/b/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+struct Factory {
+    char name[21];
+    int a;
+    int b;
+};
+
 int main() {
+    int n, x, y;
+    scanf("%d %d %d", &n, &x, &y);
+
+    struct Factory factories[500];
+    for (int i=0; i<n; i++) {
+        scanf("%s %d %d", factories[0].name, &factories[0].a, &factories[0].b);
+    }
 }
