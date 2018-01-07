ef51efc6273e48ebb5717e01e668c62084a8ee12
Jim Lin <jimlintw922@gmail.com>
2018-01-07 21:54:26 +0800

    update d

--- c/29/d/main.c
+++ c/29/d/main.c
@@ -2,4 +2,17 @@
 #include <stdbool.h>
 
 int main() {
+    int n;
+    scanf("%d", &n);
+
+    int A[n][n];
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    long long int ans;
+    // TODO cal_determinant
+    printf("%lld\n", ans);
 }
