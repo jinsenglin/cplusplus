75252c306039b5170220f5e3503c59bcac4dc853
Jim Lin <jimlintw922@gmail.com>
2018-01-07 22:08:17 +0800

    update d

--- c/29/d/main.c
+++ c/29/d/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+long long int cal_determinant(int a[8][8], int n) {
+    if (n == 2) {
+        return a[0][0]*a[1][1] - a[0][1]*a[1][0];
+    }
+    else {
+        // recursive TODO
+        return 0;
+    }
+}
+
 int main() {
     int n;
     scanf("%d", &n);
 
-    int A[n][n];
+    int a[8][8] = { 0 };
     for (int i=0; i<n; i++) {
         for (int j=0; j<n; j++) {
-            scanf("%d", &A[i][j]);
+            scanf("%d", &a[i][j]);
         }
     }
 
-    long long int ans;
-    // TODO cal_determinant
+    long long int ans = cal_determinant(a, n);
     printf("%lld\n", ans);
 }
